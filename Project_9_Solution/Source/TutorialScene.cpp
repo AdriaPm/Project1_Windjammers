c5927dcbacd416d9d579c25812f64dda0405c181
Historn <arnaujiga@gmail.com>
2022-04-25 19:54:27 +0200

    Tutorial Scene
    -Added TutorialScene.cpp and TutorialScene.h
    - Added Background Image

---
+++ Project_9_Solution/Source/TutorialScene.cpp
@@ -0,0 +1,53 @@
+#include "TutorialScene.h"
+
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleRender.h"
+#include "ModuleAudio.h"
+#include "ModuleInput.h"
+#include "ModuleFadeToBlack.h"
+
+TutorialScene::TutorialScene(bool startEnabled) : Module(startEnabled)
+{
+
+}
+
+TutorialScene::~TutorialScene()
+{
+
+}
+
+// Load assets
+bool TutorialScene::Start()
+{
+	LOG("Loading background assets");
+
+	bool ret = true;
+
+	bgTexture = App->textures->Load("Assets/Spriteswind/Sprites/UI/TutorialBG.png");
+	App->audio->PlayMusic("Assets/Music/introTitle.ogg", 1.0f);
+
+	App->render->camera.x = 0;
+	App->render->camera.y = 0;
+
+	return ret;
+}
+
+Update_Status TutorialScene::Update()
+{
+	if (App->input->keys[SDL_SCANCODE_SPACE] == Key_State::KEY_DOWN)
+	{
+		App->fade->FadeToBlack(this, (Module*)App->turflevel, 90);
+	}
+
+	return Update_Status::UPDATE_CONTINUE;
+}
+
+// Update: draw background
+Update_Status TutorialScene::PostUpdate()
+{
+	// Draw everything --------------------------------------
+	App->render->Blit(bgTexture, 0, 0, NULL);
+
+	return Update_Status::UPDATE_CONTINUE;
+}
