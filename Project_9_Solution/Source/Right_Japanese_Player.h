b2273a97f8f44e16e451beea981f4950f75b04c2
Adria Pons <adria.pons.mensa@estudiantat.upc.edu>
2022-04-27 19:22:26 +0200

    New RightJapanesePlayer module created

---
+++ Project_9_Solution/Source/Right_Japanese_Player.h
@@ -0,0 +1,89 @@
+#ifndef __Right_Japanese_Player_H__
+#define __Right_Japanese_Player_H__
+
+#include "Module.h"
+#include "Animation.h"
+#include "p2Point.h"
+
+struct SDL_Texture;
+struct Collider;
+
+class Right_Japanese_Player : public Module
+{
+public:
+	// Constructor
+	Right_Japanese_Player(bool startEnabled);
+
+	// Destructor
+	~Right_Japanese_Player();
+
+	// Called when the module is activated
+	// Loads the necessary textures for the player
+	bool Start() override;
+
+	// Called at the middle of the application loop
+	// Processes new input and handles player movement
+	Update_Status Update() override;
+
+	// Called at the end of the application loop
+	// Performs the render call of the player sprite
+	Update_Status PostUpdate() override;
+
+	// Collision callback, called when the player intersects with another collider
+	void OnCollision(Collider* c1, Collider* c2) override;
+
+public:
+	// Position of the player in the map
+	iPoint position;
+
+	// The speed in which we move the player (pixels per frame)
+	int speed = 1;
+	int speedslide = 0.75;
+	int speedcross = 0.25;
+
+	// The player spritesheet loaded into an SDL_Texture
+	SDL_Texture* texture = nullptr;
+
+	// The pointer to the current player animation
+	// It will be switched depending on the player's movement direction
+	Animation* currentAnimation = nullptr;
+
+	// A set of animations
+	Animation idleAnim;
+	Animation upAnim;
+	Animation downAnim;
+	Animation rightAnim;
+	Animation leftAnim;
+	Animation victAnim;
+	Animation loseAnim;
+	Animation SlideLAnim;
+	Animation SlideRAnim;
+	Animation SlideUAnim;
+	Animation SlideDAnim;
+	Animation UpRightAnim;
+	Animation UpLeftAnim;
+	Animation DownRightAnim;
+	Animation DownLeftAnim;
+	Animation SlideUpLeftAnim;
+	Animation SlideUpRightAnim;
+	Animation SlideDownRightAnim;
+	Animation SlideDownLeftAnim;
+
+	// The player's collider
+	Collider* collider = nullptr;
+
+	// A flag to detect when the player has been destroyed
+	bool destroyed = false;
+
+	// Sound effects indices
+	uint laserFx = 0;
+	uint explosionFx = 0;
+
+	// Font score index
+	uint score = 000;
+	int scoreFont = -1;
+	char scoreText[10] = { "\0" };
+
+};
+
+#endif //!__MODULE_PLAYER_H__
