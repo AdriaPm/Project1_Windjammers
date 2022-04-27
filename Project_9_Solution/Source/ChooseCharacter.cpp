9d96b2e41312f37c0e2e2d1f4097108dea432651
JanNetoLlorens <99729455+JanNetoLlorens@users.noreply.github.com>
2022-04-27 19:38:27 +0200

    Animation for player select
    finished

--- Project_9_Solution/Source/ChooseCharacter.cpp
+++ Project_9_Solution/Source/ChooseCharacter.cpp
@@ -9,6 +9,9 @@
 
 ChooseCharacter::ChooseCharacter(bool startEnabled) : Module(startEnabled)
 {
+	p1.PushBack({359, 11, 20, 16});
+
+	p2.PushBack({ 392, 11, 23, 16 });
 
 }
 
@@ -24,10 +27,15 @@ bool ChooseCharacter::Start()
 	bool ret = true;
 
 	//CharacterType::Turf;
+	xMove1 = 9;		xMove2 = 39;
+	yMove1 = 72;	yMove2 = 72;
+
+	currentAnimation1 = &p1;
+	currentAnimation2 = &p2;
 
 	chooseCharacterTexture = App->textures->Load("Assets/Spriteswind/Sprites/UI/ChooseCharacter.png");
-	selectCharacter = App->textures->Load("Assets/Spriteswind/Sprites/UI/UISpriteSheetFinal.png");
-	
+	playerIcon = App->textures->Load("Assets/Spriteswind/Sprites/UI/UISpriteSheetFinal.png");
+
 	App->audio->PlayMusic("Assets/Music/001 Windjammers _ Flying Power Disc (wjammers) [#002] Get Ready! (Select).ogg", 1.0f);
 
 	App->render->camera.x = 0;
@@ -38,21 +46,62 @@ bool ChooseCharacter::Start()
 
 Update_Status ChooseCharacter::Update()
 {
+	if (App->input->keys[SDL_SCANCODE_S] == Key_State::KEY_DOWN)
+	{
+		if (xMove1 == 9 && yMove1 == 72)
+		{
+			xMove1 = 9; yMove1 = 128;
+		}
+	}
+	if (App->input->keys[SDL_SCANCODE_W] == Key_State::KEY_DOWN)
+	{
+		if (xMove1 == 9 && yMove1 == 128)
+		{
+			xMove1 = 9; yMove1 = 72;
+		}
+	}
+	if (App->input->keys[SDL_SCANCODE_D] == Key_State::KEY_DOWN)
+	{
+		if (xMove1 == 9 && yMove1 == 72)
+		{
+			xMove1 =  153; yMove1 = 72;
+		}
+	}
+	if (App->input->keys[SDL_SCANCODE_A] == Key_State::KEY_DOWN)
+	{
+		if (xMove1 == 153 && yMove1 == 72)
+		{
+			xMove1 = 9; yMove1 = 72;
+		}
+	}
+
 	if (App->input->keys[SDL_SCANCODE_DOWN] == Key_State::KEY_DOWN)
 	{
-	
+		if (xMove2 == 39 && yMove2 == 72)
+		{
+			xMove2 = 39; yMove2 = 128;
+		}
 	}
 	if (App->input->keys[SDL_SCANCODE_UP] == Key_State::KEY_DOWN)
 	{
-	
+		if (xMove2 == 39 && yMove2 == 128)
+		{
+			xMove2 = 39; yMove2 = 72;
+		}
 	}
 	if (App->input->keys[SDL_SCANCODE_RIGHT] == Key_State::KEY_DOWN)
 	{
-
+		if (xMove2 == 39 && yMove2 == 72)
+		{
+			xMove2 = 183; yMove2 = 72;
+		}
 	}
 	if (App->input->keys[SDL_SCANCODE_LEFT] == Key_State::KEY_DOWN)
 	{
-
+		if (xMove2 == 183 && yMove2 == 72)
+		{
+			xMove2 = 39; yMove2 = 72;
+		}
 	}
 
 	if (App->input->keys[SDL_SCANCODE_SPACE] == Key_State::KEY_DOWN)
@@ -60,14 +109,20 @@ Update_Status ChooseCharacter::Update()
 			App->fade->FadeToBlack(this, (Module*)App->chooseMap, 40);
 	}
 
+
 	return Update_Status::UPDATE_CONTINUE;
 }
 
 // Update: draw background
 Update_Status ChooseCharacter::PostUpdate()
 {
+
+	SDL_Rect rect1 = currentAnimation1->GetCurrentFrame();
+	SDL_Rect rect2 = currentAnimation2->GetCurrentFrame();
 	// Draw everything --------------------------------------
 	App->render->Blit(chooseCharacterTexture, 0, 0, NULL);
+	App->render->Blit(playerIcon, xMove1, yMove1, &rect1);
+	App->render->Blit(playerIcon, xMove2, yMove2, &rect2);
 
 	return Update_Status::UPDATE_CONTINUE;
 }
--- Project_9_Solution/Source/ChooseCharacter.h
+++ Project_9_Solution/Source/ChooseCharacter.h
@@ -10,7 +10,9 @@ struct SDL_Texture;
 
 enum CharacterType
 {
-
+	Japanese,
+	German,
+	Spanish,
 };
 
 class ChooseCharacter : public Module
@@ -28,10 +30,17 @@ public:
 
 public:
 	SDL_Texture* chooseCharacterTexture = nullptr;
-	SDL_Texture* selectCharacter = nullptr;
+	SDL_Texture* playerIcon = nullptr;
+
+	Animation* currentAnimation1 = nullptr;
+	Animation* currentAnimation2 = nullptr;
+
+	Animation p1;
+	Animation p2;
+
 
 private:
-	int yMove, xMove;
+	int yMove1, xMove1, yMove2, xMove2;
 };
 
 #endif
