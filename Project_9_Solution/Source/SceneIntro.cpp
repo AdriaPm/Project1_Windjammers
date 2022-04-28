4238ac29a041fb427c7662dcad7b8b65de155fb0
Adria Pons <adria.pons.mensa@estudiantat.upc.edu>
2022-04-28 19:40:43 +0200

    Update SceneIntro.cpp

--- Project_9_Solution/Source/SceneIntro.cpp
+++ Project_9_Solution/Source/SceneIntro.cpp
@@ -90,7 +90,7 @@ bool SceneIntro::Start()
 
 	bool ret = true;
 
-	bgTexture = App->textures->Load("Assets/Spriteswind/Sprites/UI/Dont Use/intobg.png");
+	bgTexture = App->textures->Load("Assets/Spriteswind/Sprites/UI/Dont_Use/intobg.png");
 	App->audio->PlayMusic("Assets/Music/introTitle.ogg", 1.0f);
 
 	App->render->camera.x = 0;
