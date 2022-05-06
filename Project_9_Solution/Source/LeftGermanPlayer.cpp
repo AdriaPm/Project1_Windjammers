ecd0e9e4c4274bf70bc2846458abf253f5262068
Arnau <arnaujiga@gmail.com>
2022-05-06 11:50:46 +0200

    change

--- Project_9_Solution/Source/LeftGermanPlayer.cpp
+++ Project_9_Solution/Source/LeftGermanPlayer.cpp
@@ -361,24 +361,26 @@ void LeftGermanPlayer::OnCollision(Collider* c1, Collider* c2)
 		}
 	}
 
-	if (c1->type == Collider::Type::DISK && c2->type == Collider::Type::UPPER_WALL)
+	if (c1->type == Collider::Type::DISK && c2->type == Collider::Type::UPPER_WALL && c2->type == Collider::Type::NET)
 	{
-		App->particles->diskL.speed.x = 1.0f;
-		App->particles->diskL.speed.y = -3.5f;
-		App->particles->diskL.position.x += App->particles->diskL.speed.x;
-		App->particles->diskL.position.y += App->particles->diskL.speed.y;
+		
 		Particle* newParticle = App->particles->AddParticle(App->particles->diskL, c1->rect.x, c1->rect.y, Collider::Type::DISK);
 		newParticle->collider->AddListener(this);
-	}
-
-	if (c1->type == Collider::Type::DISK && c2->type == Collider::Type::LOWER_WALL)
-	{
 		App->particles->diskL.speed.x = 1.0f;
 		App->particles->diskL.speed.y = 3.5f;
 		App->particles->diskL.position.x += App->particles->diskL.speed.x;
 		App->particles->diskL.position.y += App->particles->diskL.speed.y;
+	}
+
+	if (c1->type == Collider::Type::DISK && c2->type == Collider::Type::LOWER_WALL)
+	{
+		
 		Particle* newParticle = App->particles->AddParticle(App->particles->diskL, c1->rect.x, c1->rect.y, Collider::Type::DISK);
 		newParticle->collider->AddListener(this);
+		App->particles->diskL.speed.x = 1.0f;
+		App->particles->diskL.speed.y = -3.5f;
+		App->particles->diskL.position.x += App->particles->diskL.speed.x;
+		App->particles->diskL.position.y += App->particles->diskL.speed.y;
 	}
 	
 	if (c1->type == Collider::Type::PLAYER && c2->type == Collider::Type::DISK)
